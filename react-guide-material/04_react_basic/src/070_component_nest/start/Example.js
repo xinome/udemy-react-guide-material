638e407a6e0bfb377efebd988aa8c3dfe1463f26
xinome <xinome2038@gmail.com>
2023-11-22 16:04:05 +0900

    27. コンポーネントの分割方法

--- react-guide-material/04_react_basic/src/070_component_nest/start/Example.js
+++ react-guide-material/04_react_basic/src/070_component_nest/start/Example.js
@@ -1,11 +1,8 @@
-import "./Example.css";
+import "./components/Child.css"
+import Child from "./components/Child";   // export defaultとしたコンポーネントは、import時に任意の名前でimportできる
 
 const Example = () => {
-  return (
-    <div className="component">
-      <h3>Hello Component</h3>
-    </div>
-  );
+  return <Child />;
 };
 
 export default Example;
