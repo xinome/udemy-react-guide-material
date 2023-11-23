627a0a61500b10eb087ade6d2fbc2b1755bc913c
xinome <xinome2038@gmail.com>
2023-11-23 15:31:49 +0900

    44. イベントに合わせて画面表示を更新してみよう

--- react-guide-material/05_state_and_event/src/020_useState/start/Example.js
+++ react-guide-material/05_state_and_event/src/020_useState/start/Example.js
@@ -1,8 +1,19 @@
+import { useState } from "react";
+
 const Example = () => {
+
+  let [val, setVal] = useState(0);
+  // console.log(val, setVal);
 
   return (
     <>
-      
+      <input
+        type="text"
+        onChange={(e) => {
+          // const setFn = valArray[1];
+          setVal(e.target.value);
+        }}
+      /> = {val}
     </>
   );
 };
