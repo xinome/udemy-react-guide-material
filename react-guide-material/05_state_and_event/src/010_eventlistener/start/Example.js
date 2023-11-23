b5f5a6012ca985b52b65d9dc0d9934da68ebf8ad
xinome <xinome2038@gmail.com>
2023-11-23 15:10:32 +0900

    42. イベントに合わせて関数を実行してみよう

--- react-guide-material/05_state_and_event/src/010_eventlistener/start/Example.js
+++ react-guide-material/05_state_and_event/src/010_eventlistener/start/Example.js
@@ -1,6 +1,14 @@
 const Example = () => {
+
+  const clickHandler = () => {
+    alert('クリックされました');
+  };
+
   return (
-    <></>
+    <>
+      <button onClick={clickHandler}>クリックしてね</button>
+      <button>クリックしてね</button>
+    </>
   );
 };
 
