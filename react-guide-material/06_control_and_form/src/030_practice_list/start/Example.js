ddc9acf400d672851fe4a390d0dd41c5034afed8
xinome <xinome2038@gmail.com>
2023-11-27 15:45:33 +0900

    61. 【練習】リストにキーを設定してみよう

--- react-guide-material/06_control_and_form/src/030_practice_list/start/Example.js
+++ react-guide-material/06_control_and_form/src/030_practice_list/start/Example.js
@@ -1,3 +1,23 @@
+import Profile from "./components/Profile";
+
+const persons = [
+  {
+    name: "Geo",
+    age: 18,
+    hobbies: ["sports", "music"],
+  },
+  {
+    name: "Tom",
+    age: 25,
+    hobbies: ["movie", "music"],
+  },
+  {
+    name: "Lisa",
+    age: 21,
+    hobbies: ["sports", "travel", "game"],
+  },
+];
+
 const Example = () => {
   return (
     <>
@@ -5,7 +25,15 @@ const Example = () => {
       <p>Profileコンポーネントを使用して、完成コードと同じ画面を作成してください。</p>
       <p>また、Profileコンポーネント内のリスト表示部分にkeyを設定して、ワーニング表示がされないようにしてください。</p>
       <ul>
-        {/* ここに記述 */}
+        {persons.map((person) => 
+          <li key={person.name}>
+            <Profile
+              name={person.name}
+              age={person.age}
+              hobbies={person.hobbies}
+            />
+          </li>
+        )}
       </ul>
     </>
   );
