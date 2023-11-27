4a14898c7d1807726a9365da46a77d6f45135c4f
xinome <xinome2038@gmail.com>
2023-11-27 15:15:11 +0900

    59. 配列をリスト表示

--- react-guide-material/06_control_and_form/src/010_list_components/start/Example.js
+++ react-guide-material/06_control_and_form/src/010_list_components/start/Example.js
@@ -1,13 +1,26 @@
 
 const animals = ["Dog", "Cat", "Rat"];
 
+// オブジェクト型の場合
+const animalList = [];
+for(const animal of animals) {
+  animalList.push(<li>{animal}</li>);
+}
+
+// マップの場合
+const helloAnimals = animals.map((animal) => (
+  <li>Hello, {animal}</li>
+));
+
 const Example = () => {
 
   return (
     <>
       <h3>配列の操作</h3>
       <ul>
-        
+        {/* {animalList} */}
+        {/* {helloAnimals} */}
+        {animals.map((animal) => <li>Hello, {animal}</li> )}
       </ul>
     </>
   );
