ed45cb1163f8488a081be2a1440fd71f22fa5da8
xinome <xinome2038@gmail.com>
2023-11-30 17:40:49 +0900

    87. 【練習＆解答】createPortalでトーストを作成してみよう

--- react-guide-material/08_other_function/src/010_portals/start/Example.js
+++ react-guide-material/08_other_function/src/010_portals/start/Example.js
@@ -23,7 +23,7 @@ const Example = () => {
   const [modalOpen, setModalOpen] = useState(false);
   return (
     <div onClick={() => console.log('空のdiv')}>
-      <div className="container start" onClick={() => console.log('container')}>></div>
+      <div className="container start" onClick={() => console.log('container')}></div>
 
       <button
         type="button"
