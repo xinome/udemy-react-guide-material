ed0b1ea56edead2ac9a831b0b3abcc075627bc52
xinome <xinome2038@gmail.com>
2023-11-30 14:13:52 +0900

    73. インラインスタイルの使い方！

--- react-guide-material/07_styling_component/src/010_inline_style/start/Example.js
+++ react-guide-material/07_styling_component/src/010_inline_style/start/Example.js
@@ -5,10 +5,26 @@ const Example = () => {
 
     const clickHandler = () => setIsSelected(prev => !prev);
 
+    const style = {
+        width: 120,
+        height: 60,
+        display: "block",
+        fontWeight: "bold",     // font-weight -> fontWeight
+        "border-radius":  9999,     // border-radius -> "border-radius"
+        cursor: "pointer",
+        border: "none",
+        margin: "auto",
+
+        // 条件によってスタイルを変更する
+        background: isSelected ? "pink" : "",
+    };
+
     return (
         <>
-            <button onClick={clickHandler}>ボタン</button>
-            <div>{isSelected && "クリックされました。"}</div>
+            <button onClick={clickHandler} style={style}>ボタン</button>
+            <div style={{
+                textAlign: "center",
+            }}>{isSelected && "クリックされました。"}</div>
         </>
     )
 };
