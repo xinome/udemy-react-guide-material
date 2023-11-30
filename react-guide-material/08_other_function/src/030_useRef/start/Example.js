a77cac5cd99f435eb79ab4b279e9ba6c3cb3e96f
xinome <xinome2038@gmail.com>
2023-11-30 17:47:53 +0900

    88. 【useRef】refでDOMを直接操作してみよう

--- react-guide-material/08_other_function/src/030_useRef/start/Example.js
+++ react-guide-material/08_other_function/src/030_useRef/start/Example.js
@@ -1,13 +1,16 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 const Case1 = () => {
   const [value, setValue] = useState("");
+  const inputRef = useRef();
+
+  console.log(inputRef);
 
   return (
     <div>
       <h3>ユースケース1</h3>
-      <input type="text" value={value} onChange={(e) => setValue(e.target.value)} />
-      <button onClick>
+      <input type="text" ref={inputRef} value={value} onChange={(e) => setValue(e.target.value)} />
+      <button onClick={() => inputRef.current.focus()}>
         インプット要素をフォーカスする
       </button>
     </div>
