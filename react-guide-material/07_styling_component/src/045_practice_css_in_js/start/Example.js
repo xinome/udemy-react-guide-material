7f631c89bf568b77bd5257d60eb8c7338debf39e
xinome <xinome2038@gmail.com>
2023-11-30 15:42:15 +0900

    79. 【練習&解答】styled-components

--- react-guide-material/07_styling_component/src/045_practice_css_in_js/start/Example.js
+++ react-guide-material/07_styling_component/src/045_practice_css_in_js/start/Example.js
@@ -7,6 +7,18 @@ const FirstButton = styled.button`
   margin: 0.5rem 1rem;
   width: 11rem;
   border: none;
+
+  background: pink;
+`;
+
+const SecondButton = styled(FirstButton)`
+  background: red;
+  color: white;
+`;
+
+const ThirdButton = styled(SecondButton)`
+  /* background: ${(props) => props.dark ? 'black' : 'green'}; */
+  background: ${({ dark }) => dark ? 'black' : 'green'};
 `;
 
 const Example = () => {
@@ -28,6 +40,9 @@ const Example = () => {
         </ul>
       </p>
       <FirstButton>ボタン1</FirstButton>
+      <SecondButton>ボタン2</SecondButton>
+      <ThirdButton>ボタン3</ThirdButton>
+      <ThirdButton dark>ボタン3</ThirdButton>
     </>
   );
 };
