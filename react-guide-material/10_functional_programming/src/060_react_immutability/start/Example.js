262d2e8e007eb9ad4d82923b44b7b204ce1d8df8
xinome <xinome2038@gmail.com>
2023-12-06 16:13:31 +0900

    108. Reactにおける不変性

--- react-guide-material/10_functional_programming/src/060_react_immutability/start/Example.js
+++ react-guide-material/10_functional_programming/src/060_react_immutability/start/Example.js
@@ -1,9 +1,19 @@
 import { useState } from "react";
 
-const Child = (props) => {
+const Child = ({ state, setState }) => {
+  // setState({ value: 1 });
+
+  const increment = () => {
+    setState(prev => {
+      const newState = { value: prev.value + 1 };
+      return newState;
+    })
+  }
+
   return (
     <>
-      <span>{props.state.value}</span>
+      <span>{state.value}</span>
+      <button onClick={increment}>+</button>
     </>
   );
 };
@@ -14,7 +24,7 @@ const Example = () => {
   return (
     <>
       <div>
-        <Child state={state}/>
+        <Child state={state} setState={setState} />
       </div>
     </>
   );
