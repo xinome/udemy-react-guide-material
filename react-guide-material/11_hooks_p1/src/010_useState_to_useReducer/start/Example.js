bb6b92f4a5563293de478ed64be494657c050653
xinome <xinome2038@gmail.com>
2023-12-06 16:45:34 +0900

    112. useReducerを使ってみよう

--- react-guide-material/11_hooks_p1/src/010_useState_to_useReducer/start/Example.js
+++ react-guide-material/11_hooks_p1/src/010_useState_to_useReducer/start/Example.js
@@ -1,7 +1,39 @@
+import { useReducer, useState } from "react";
+
 const Example = () => {
+
+  // useStateの書き換え
+  const [state, setState] = useState(0);
+  const countUp = () => {
+    setState((prev) => ++prev);
+  };
+
+  // useReducerの書き換え
+  const [rstate, dispatch] = useReducer((prev, action) => {
+    if(action === "+") {
+      return ++prev;
+    } else {
+      return --prev;
+    }
+  }, 0);
+  const rcounUp = () => {
+    dispatch("+");
+  };
+  const rcounDown = () => {
+    dispatch("-");
+  };
   
   return (
     <>
+      <div>
+        <h3>{state}</h3>
+        <button onClick={countUp}>+</button>
+      </div>
+      <div>
+        <h3>{rstate}</h3>
+        <button onClick={rcounUp}>+</button>
+        <button onClick={rcounDown}>-</button>
+      </div>
     </>
   );
 };
