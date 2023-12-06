b507edd0579714eb72492a554e896537f276beda
xinome <xinome2038@gmail.com>
2023-12-06 15:55:14 +0900

    106. Reactと純粋関数

--- react-guide-material/10_functional_programming/src/040_react_pure_fn/start/Example.js
+++ react-guide-material/10_functional_programming/src/040_react_pure_fn/start/Example.js
@@ -1,7 +1,29 @@
+// POINT Reactと純粋関数
+let value = 0;
+
+const Child = () => {
+  value++;
+  return <div>{value}</div>
+}
+
+const ChildPure = ({ value }) => {
+  return <div>{value}</div>
+}
+
 const Example = () => {
+  let value = 0;
 
   return (
     <>
+      {/* Bad: valueの値が変わるため、特定の引数に対して同じ戻り値が返ってこない */}
+      <Child />
+      <Child />
+      <Child />
+
+      {/* Good: 引数をpropsとして渡しているため、純粋関数となる */}
+      <ChildPure value={++value} />
+      <ChildPure value={++value} />
+      <ChildPure value={++value} />
     </>
   );
 };
