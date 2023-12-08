3750d68ef4358c8713e0ba9f79841e107b8d7a82
xinome <xinome2038@gmail.com>
2023-12-08 11:04:14 +0900

    129. useEffectのクリーンアップ処理の使い方

--- react-guide-material/12_hooks_p2/src/030_useEffect_cleanup/start/Example.js
+++ react-guide-material/12_hooks_p2/src/030_useEffect_cleanup/start/Example.js
@@ -1,18 +1,41 @@
 import { useEffect, useState } from "react";
 
 const Example = () => {
+  const [isDisp, setIsDisp] = useState(true);
+
+  return (
+    <>
+      {isDisp && <Timer />}
+      <button onClick={() => setIsDisp(prev => !prev)}>トグル</button>
+    </>
+  );
+};
+
+const Timer = () => {
   const [time, setTime] = useState(0);
 
   useEffect(() => {
-    console.log('useEffect is called');
+    console.log('init');
     window.setInterval(() => {
       setTime(prev => prev + 1);
     }, 1000);
+    
+    // コールバック関数を返すことで、クリーンアップ関数を実装できる
+    // Timerコンポーネントがアンマウントされるときに実行される
+    return () => {
+      console.log('end');
+    };
   }, [])
   
   useEffect(() => {
+    console.log('updated');
+
     document.title = 'counter:' + time;
-    window.localStorage.setItem('time-key-end', time)
+    window.localStorage.setItem('time-key-end', time);
+
+    return () => {
+      console.log('updated end');
+    };
   }, [time]);
 
   return (
