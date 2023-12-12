3048df09e989d985b9e40e4b78323b962e905da7
xinome <xinome2038@gmail.com>
2023-12-12 14:13:35 +0900

    141. 複数のReducerを使う方法

--- react-guide-material/13_redux/start/src/015_multiple_reducers/components/CounterResult.js
+++ react-guide-material/13_redux/start/src/015_multiple_reducers/components/CounterResult.js
@@ -1,9 +1,12 @@
 import { useCounter } from "../context/CounterContext";
 import { useSelector } from "react-redux"
+
 const CounterResult = () => {
   // const state = useCounter();
   const state = useSelector(state => state);
-  return <h3>{state}</h3>;
+  console.log(state);
+
+  return <h3>{state.counter} : {state.counter2}</h3>;
 };
 
 export default CounterResult;
