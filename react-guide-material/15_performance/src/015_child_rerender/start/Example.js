b1d22f32d0d3d5e22e3f304ab5aa291c88d25e80
xinome <xinome2038@gmail.com>
2023-12-13 15:36:50 +0900

    161. 子コンポーネントの不要なレンダリング

--- react-guide-material/15_performance/src/015_child_rerender/start/Example.js
+++ react-guide-material/15_performance/src/015_child_rerender/start/Example.js
@@ -1,10 +1,48 @@
+// POINT 子コンポーネントの再レンダリング
+import React, { useState } from "react";
+import Child from "./Child";
+import "./Example.css";
+
 const Example = () => {
+  console.log("Parent render");
+  const [countA, setCountA] = useState(0);
+  const [countB, setCountB] = useState(0);
+
   return (
-    <>
-    <h3>再レンダリング</h3>
-    <p>親コンポーネントが再レンダリングされると子コンポーネントも再レンダリングされる</p>
-    <p>コンソールを要確認</p>
-    </>
+    // <>
+    // <h3>再レンダリング</h3>
+    // <p>親コンポーネントが再レンダリングされると子コンポーネントも再レンダリングされる</p>
+    // <p>コンソールを要確認</p>
+    // </>
+    <div className="parent">
+      <div>
+        <h3>親コンポーネント領域</h3>
+        <div>
+          <button
+            onClick={() => {
+              setCountA((pre) => pre + 1);
+            }}
+          >
+            ボタンA
+          </button>
+          <span>親のstateを更新</span>
+        </div>
+        <div>
+          <button
+            onClick={() => {
+              setCountB((pre) => pre + 1);
+            }}
+          >
+            ボタンB
+          </button>
+          <span>子のpropsに渡すstateを更新</span>
+        </div>
+      </div>
+      <div>
+        <p>ボタンAクリック回数：{countA}</p>
+      </div>
+      <Child countB={countB} />
+    </div>
   );
 };
 
