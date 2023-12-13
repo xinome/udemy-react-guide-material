8e1bd3b023d8165a91eebcd02599761b3f9bd05d
xinome <xinome2038@gmail.com>
2023-12-13 16:27:22 +0900

    165. useMemoを使った値のメモ化

--- react-guide-material/15_performance/src/050_useMemo/start/Child.js
+++ react-guide-material/15_performance/src/050_useMemo/start/Child.js
@@ -1,15 +1,27 @@
-import React from "react";
+import { useMemo } from "react";
 
-const Child = React.memo(({ countB, onClick }) => {
+const Child = ({ countB, onClick }) => {
   console.log("%cChild render", "color: red;");
 
-  return (
-    <div className="child">
-      <h2>子コンポーネント領域</h2>
-      <button onClick={onClick}>ボタンB</button>
-      <p>ボタンBクリック回数：{countB}</p>
-    </div>
-  );
-});
+  // return (
+  //   <div className="child">
+  //     <h2>子コンポーネント領域</h2>
+  //     <button onClick={onClick}>ボタンB</button>
+  //     <p>ボタンBクリック回数：{countB}</p>
+  //   </div>
+  // );
+
+  return useMemo(() => {
+    console.log("%cuseMemo", "color: green;");
+    return (
+      <div className="child">
+        <h2>子コンポーネント領域</h2>
+        <button onClick={onClick}>ボタンB</button>
+        <p>ボタンBクリック回数：{countB}</p>
+      </div>
+    );
+  }, [countB, onClick]);
+  
+};
 
 export default Child;
