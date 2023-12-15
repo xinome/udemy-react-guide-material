4e7e97633ed3ac8117880574edfe9d9179bbf2be
xinome <xinome2038@gmail.com>
2023-12-15 12:12:04 +0900

    178. 【発展】ダイナミックインポートとは？

--- react-guide-material/16_rest_api/src/070_dynamic_import/start/Example.js
+++ react-guide-material/16_rest_api/src/070_dynamic_import/start/Example.js
@@ -1,5 +1,17 @@
+// 通常のimport
+import { add } from './add';
+
 const Example = () => {
-  
+  // ダイナミックインポート
+  import('./add').then((module) => {
+    console.log(module);
+  });
+
+  import('./add').then(({ add }) => {
+    console.log(add(1, 2));
+  });
+
+  console.log(add(1, 2));
 };
 
 export default Example;
