64a585c7694f8c34374ceae380b4866554834527
xinome <xinome2038@gmail.com>
2023-12-15 10:59:27 +0900

    173. Axiosを使ってサーバーからデータを取得しよう

--- react-guide-material/16_rest_api/src/040_axios_get_request/start/Example.js
+++ react-guide-material/16_rest_api/src/040_axios_get_request/start/Example.js
@@ -1,4 +1,26 @@
+import { useEffect } from "react";
+import axios from "axios";
+
+// axiosでリクエストを送信
 const Example = () => {
+  // useEffect(() => {
+  //   axios.get('http://localhost:3003/user').then((res)  => {
+  //     console.log("res: ", res);
+  //     console.log("res.data: ", res.data);
+  //   });
+  // });
+
+  // awaitを使う場合は、asyncをつける
+  useEffect(() => {
+    const getUser = async () => {
+      const res = await axios.get('http://localhost:3003/user');
+      console.log("res: ", res);
+      console.log("res.data: ", res.data);
+    };
+    getUser();
+  });
+
+
   return <></>;
 };
 
