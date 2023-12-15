143d9f91ad82f9bee46c3068da08c52af93cef63
xinome <xinome2038@gmail.com>
2023-12-15 11:11:14 +0900

    174. 取得したデータを画面に反映してみよう

--- react-guide-material/16_rest_api/src/050_axios_get_state/start/Example.js
+++ react-guide-material/16_rest_api/src/050_axios_get_state/start/Example.js
@@ -1,5 +1,33 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 const Example = () => {
-  return <></>;
+
+  // JSONデータを格納するためのstateは空配列で初期化
+  const [users, setUsers] = useState([]);
+
+  // サーバーから取得したデータを画面表示
+  useEffect(() => {
+    const getUser = async () => {
+      const res = await axios.get("http://localhost:3003/user");
+      setUsers(res.data);
+    };
+    getUser();
+  }, []);
+
+  return (
+    <div>
+      {users.map(user => {
+        return (
+          <div key={user.id}>
+            <h3>{user.username}</h3>
+            <p>{user.age}</p>
+            <p>{user.hobbies.join(", ")}</p>
+          </div>
+        )
+      })}
+    </div>
+  );
 };
 
 export default Example;
