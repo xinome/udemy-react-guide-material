e75092f2639fcc773dbc56a164ecb0a9aa4bc620
xinome <xinome2038@gmail.com>
2023-12-20 15:55:45 +0900

    189. Linkを使って画面遷移を行おう

--- react-guide-material/17_nextjs_p1/start/src/pages/index.js
+++ react-guide-material/17_nextjs_p1/start/src/pages/index.js
@@ -1,5 +1,17 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
-    <h1>Home</h1>
+    <>
+      <h1>Home</h1>
+      {/* <Link href="/07_router"> */}
+      {/* <Link href="/07_router" as="/dummy-url"> */}
+      <Link href={{
+        pathname: "/07_router",
+        query: { key1: "value1" }
+      }} >
+        <a>/07_router</a>
+      </Link>
+    </>
   )
 }
