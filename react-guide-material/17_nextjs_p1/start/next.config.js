2ab571fb09f239b003e5d2fbf9cf9d78e6d46bae
xinome <xinome2038@gmail.com>
2023-12-21 10:46:13 +0900

    190. シングルコンポーネントで複数画面を作成する方法

--- react-guide-material/17_nextjs_p1/start/next.config.js
+++ react-guide-material/17_nextjs_p1/start/next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // アクセスがあった際に、どのページに飛ばすかを設定する
+  async rewrites() {
+    return [
+      {
+        source: "/personal",
+        destination: "/08_multipage?step=1",
+      },
+      {
+        source: "/confirm",
+        destination: "/08_multipage?step=2",
+      },
+    ];
+  }
 }
 
 module.exports = nextConfig
