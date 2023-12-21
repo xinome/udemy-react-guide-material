080e74a523caa6c51707d5db8e25f537b94675ed
xinome <xinome2038@gmail.com>
2023-12-21 12:20:14 +0900

    193. 【_app.js】サイト全体に設定を加える方法について学ぼう

--- react-guide-material/17_nextjs_p1/start/src/pages/_app.js
+++ react-guide-material/17_nextjs_p1/start/src/pages/_app.js
@@ -1,10 +1,18 @@
 import '../styles/globals.css'
 import { AppProvider } from '../context/AppContext'
+import Layout1 from '../components/layout/layout1'
 
 function MyApp({ Component, pageProps }) {
+  // ここでレイアウトを指定する
+  Component.getLayout = Component.getLayout ?? ((page) => 
+    <Layout1>{page}</Layout1>
+  )
+
   return (
     <AppProvider>
-      <Component {...pageProps} />
+      <Layout1>
+        <Component {...pageProps} />
+      </Layout1>
     </AppProvider>
   )
 }
